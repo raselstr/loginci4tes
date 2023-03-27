888307c43fa4926b65a5355e37f42db3b9706919
Rasel S Str <54805845+raselstr@users.noreply.github.com>
2023-03-27 16:19:34 +0700

    Coba Aktivasi Email

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -21,6 +21,9 @@ class Filters extends BaseConfig
         'honeypot'      => Honeypot::class,
         'invalidchars'  => InvalidChars::class,
         'secureheaders' => SecureHeaders::class,
+        'login'         => \Myth\Auth\Filters\LoginFilter::class,
+        'role'          => \Myth\Auth\Filters\RoleFilter::class,
+        'permission'    => \Myth\Auth\Filters\PermissionFilter::class,
     ];
 
     /**
@@ -29,9 +32,10 @@ class Filters extends BaseConfig
      */
     public array $globals = [
         'before' => [
-            // 'honeypot',
+            'honeypot',
             // 'csrf',
             // 'invalidchars',
+            'login'
         ],
         'after' => [
             'toolbar',
