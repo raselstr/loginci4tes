6e04ac2f43452be7ce2662925132a0caf9ca6d4e
Rasel S Str <54805845+raselstr@users.noreply.github.com>
2023-03-27 13:54:29 +0700

    Myth_Auth

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInit364a4994f49e6a59facb25e47f46a236
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit364a4994f49e6a59facb25e47f46a236::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
